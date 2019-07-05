6c058fb5ed2ce4e11e2ceb53ac1d664f06714844
Nenye <chinenyediei@gmail.com>
2019-07-04 19:02:20 -0500

    database created and site is connected at local host 8080

---
+++ javascript/bamazonCustomer.js
@@ -0,0 +1,35 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var express = require("express");
+
+var app = express();
+
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  PORT: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "Topper333",
+  database: "bamazon_db"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("404 Error"); 
+});
+
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  
