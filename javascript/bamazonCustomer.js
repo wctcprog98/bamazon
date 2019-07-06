bd0938e6a5ce6c43810b7a53718ebcc6a01ad2d8
Nenye <chinenyediei@gmail.com>
2019-07-06 03:31:50 -0500

    table now displays products available to customer

--- javascript/bamazonCustomer.js
+++ javascript/bamazonCustomer.js
@@ -2,6 +2,9 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 var express = require("express");
 var app = express();
+//Use table to display table
+var Table = require("cli-table");
+
 // process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8080
 // create the connection information for the sql database
@@ -25,24 +28,46 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
   if (err) throw err;
   console.log("Connected as id: " + connection.threadId); 
-
-  afterConnection(); 
+  displayProducts(); 
+  // afterConnection(); 
 });
 
-function afterConnection() {
-  connection.query("SELECT * FROM products", function (err, res) {
-    if (err) throw err;
-    console.log(res);
-    connection.end
-  })
-}
+// function afterConnection() {
+//   connection.query("SELECT * FROM products", function (err, res) {
+//     if (err) throw err;
+//     console.log(res);
+//     connection.end
+//   })
+// }
 
 app.listen(PORT, function() {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
 });
 
-  
+//display PRODUCTS AVAILABLE.
+function displayProducts() {	
+	connection.query("Select * FROM products", function(err, res){
+		if(err) throw err;
+		var displayTable = new Table ({
+			head: ["Item ID", "Product Name", "Department Name", "Price", "Stock Quantity"],
+			colWidths: [10,25,25,10,14]
+    });
+    
+		for(var i = 0; i < res.length; i++){
+      displayTable.push(
+        // product_name, department_name, price, stock_quantitiy
+				[res[i].id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+      );
+     
+    }
+    
+    console.log(displayTable.toString());
+    // purchasePrompt();
+    connection.end
+	});
+}
+
 // var typeOfProduct = function () {
 //   inquirer.prompt([{
 //     message: "What's the id of the product you would like to purchase? ",
@@ -51,7 +76,7 @@ app.listen(PORT, function() {
 //   }, {
 //     message: "What quantity would you like to purchase? ",
 //     type: "input",
-//     name: "productID"
+//     name: "quantity"
 //   },
 //   ])
 // }
--- schema.sql
+++ schema.sql
@@ -4,44 +4,44 @@ create database bamazon_db;
 
 use bamazon_db; 
 
-create table products (
-id integer(10) not null auto_increment,
-product_name varchar(100) not null,
-department_name varchar(40) not null,
-price integer(10) not null,
-stock_quantitiy integer(10) not null,
-primary key(id)
+CREATE TABLE products (
+id INTEGER(10) not null auto_increment ,
+product_name VARCHAR(100) not null,
+department_name VARCHAR(40) not null,
+price DECIMAL (10,2) not null,
+stock_quantitiy INTEGER(10) not null,
+PRIMARY KEY(id)
 
 );
 
-insert into products (product_name, department_name, price, stock_quantitiy)
-values("Bamazon Echo", "Electronics", 24.99, 1000);
+INSERT INTO products (product_name, department_name, price, stock_quantitiy)
+VALUES("Bamazon Echo", "Electronics", 24.99, 1000);
 
-insert into products(product_name, department_name, price, stock_quantitiy)
-values("The Language of SQL", "Books",29.99, 500);
+INSERT INTO products(product_name, department_name, price, stock_quantitiy)
+VALUES("The Language of SQL", "Books",29.99, 500);
 
-insert into products(product_name, department_name, price, stock_quantitiy)
-values("television mount", "Electronics Accessories", 24.99, 100);
+INSERT INTO products(product_name, department_name, price, stock_quantitiy)
+VALUES("television mount", "Electronics Accessories", 24.99, 100);
 
-insert into products(product_name, department_name, price, stock_quantitiy)
-values("DLP Projector", "Electronics", 324.99, 50);
+INSERT INTO products(product_name, department_name, price, stock_quantitiy)
+VALUES("DLP Projector", "Electronics", 324.99, 50);
 
-insert into products(product_name, department_name, price, stock_quantitiy)
+INSERT INTO products(product_name, department_name, price, stock_quantitiy)
 values("Paint Brush 12 pack", "Arts and Crafts", 10.00, 200);
 
-insert into products(product_name, department_name, price, stock_quantitiy)
+INSERT INTO products(product_name, department_name, price, stock_quantitiy)
 values("Dryer Balls", "Household", 14.99, 10000);
 
-insert into products(product_name, department_name, price, stock_quantitiy)
+INSERT INTO products(product_name, department_name, price, stock_quantitiy)
 values("Bamazon Echo", "Electronics", 24.99, 1000);
 
-insert into products(product_name, department_name, price, stock_quantitiy)
+INSERT INTO products(product_name, department_name, price, stock_quantitiy)
 values("Chicken Breast", "Grocery", 4.99, 250);
 
-insert into products(product_name, department_name, price, stock_quantitiy)
+INSERT INTO products(product_name, department_name, price, stock_quantitiy)
 values("MVMT silver watch", "Fashion", 114.99, 20);
 
-insert into products(product_name, department_name, price, stock_quantitiy)
+INSERT INTO products(product_name, department_name, price, stock_quantitiy)
 values("Bamazon Echo", "Electronics", 24.99, 1000);
 
 
