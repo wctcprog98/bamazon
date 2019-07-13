01375f80eb6dd7bd72965e37b5478731aa83d3a6
Nenye <chinenyediei@gmail.com>
2019-07-13 02:11:25 -0500

    Calculates total price, quantity is validated to see if enough is available

--- javascript/bamazonCustomer.js
+++ javascript/bamazonCustomer.js
@@ -28,77 +28,141 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
   if (err) throw err;
   console.log("Connected as id: " + connection.threadId);
-  displayProducts(); 
+  displayProducts();
   // afterConnection(); 
 });
 
 
 //display PRODUCTS AVAILABLE.
-function displayProducts() {	
-	connection.query("Select * FROM products", function(err, res){
-		if(err) throw err;
-		var displayTable = new Table ({
-			head: ["Item ID", "Product Name", "Department Name", "Price", "Stock Quantity"],
-			colWidths: [10,25,25,10,14]
+function displayProducts() {
+  connection.query("Select * FROM products", function (err, res) {
+    if (err) throw err;
+    var displayTable = new Table({
+      head: ["Item ID", "Product Name", "Department Name", "Price", "Stock Quantity"],
+      colWidths: [10, 25, 25, 10, 14]
     });
-    
-		for(var i = 0; i < res.length; i++){
+
+    for (var i = 0; i < res.length; i++) {
       displayTable.push(
         // product_name, department_name, price, stock_quantitiy
-				[res[i].id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+        [res[i].id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
       );
     }
     console.log(displayTable.toString());
-    productPrompt(); 
-    
-	});
+    // productPrompt(); 
+    userChoice();
+  });
 }
 
-//prompt user for id# of item they want to purchase
-var productPrompt = function (res) {
-  inquirer.prompt([{
-    message: "What? product you would like to purchase ",
-    type: "input",
-    name: "productID",
-    validate: function (value) {
-      if (isNaN(value) === false) {
-        return true;
-      }
-      console.log(chalk.red('Please enter a number from 1-10'));
-      return false;
-    }
-  }, {
-    //prompt user for quantity of the item they want to purchase
-    message: "What quantity would you like to purchase? ",
-    type: "input",
-    name: "quantity",
-    validate: function (value) {
-      if (isNaN(value) === false) {
-        return true;
-      }
-      console.log(chalk.red('Please enter a number from 1-10')); //red text if NaN
-      return false;
-    }
-  },
-  {
-    type: "confirm",
-    message: "Are you sure?",
-    name: "confirm",
-    default: true
-  },
-  ]).then(function (answers) {
-    
-    var quantityNeeded = answers.quantity;
-    var idNeeded = answers.productID;
-    console.log(idNeeded);
-    
-    connection.query("SELECT * FROM products WHERE id = " + idNeeded, function (err, res) {
-      if (err) throw err;
-      console.log("Here is your order information and our current stock.");
-      console.log(res);
-    });
-    })
+function userChoice() {
+  // query the database for all items
+  connection.query("SELECT * FROM products", function (err, results) {
+    if (err) throw err;
+    inquirer
+      .prompt([
+        {
+          name: "productID",
+          type: "rawlist",
+          choices: function () {
+            var choiceArray = [];
+            for (var i = 0; i < results.length; i++) {
+              choiceArray.push(results[i].product_name);
+            }
+            return choiceArray;
+          },
+          message: "What product you would like to purchase?"
+        },
+        {
+          type: "input",
+          name: "quantity",
+          message: "What quantity would you like to purchase?",
+          validate: function (value) {
+                  if (isNaN(value) === false) {
+                    return true;
+                  }
+                  console.log(chalk.red('Please enter a number from 1-10')); //red text if NaN
+                  return false;
+                }
+        }
+      ])
+      .then(function (answer) {
+        var quantityNeeded = answer.quantity;
+        var idNeeded = answer.productID;
+
+        // get the information of the chosen item
+        var chosenItem;
+        for (var i = 0; i < results.length; i++) {
+          if (results[i].product_name === idNeeded) {
+            chosenItem = results[i];
+
+          }
+        }
+        console.log(chosenItem);
+
+        // determine if enough stock on hand
+        if (quantityNeeded < chosenItem.stock_quantity) {
+          console.log(quantityNeeded);
+          console.log(chosenItem.stock_quantity);
+          console.log("You are in luck we have that product in stock");
+          //calculte total purchase price
+          var productPrice = chosenItem.price; 
+              var cost = quantityNeeded * productPrice;
+            console.log("Your total is $" + cost);
+          }
+        else {
+            // bid wasn't high enough, so apologize and start over
+          console.log("We don't have enough stock available please enter a smaller quantity...");
+          }
+        });
+  });
 }
+//update stock in db
+
+// //prompt user for id# of item they want to purchase
+// var productPrompt = function (res) {
+//   inquirer.prompt([{
+//     message: "What? product you would like to purchase ",
+//     type: "input",
+//     name: "productID",
+//     validate: function (value) {
+//       if (isNaN(value) === false) {
+//         return true;
+//       }
+//       console.log(chalk.red('Please enter a number from 1-10'));
+//       return false;
+//     }
+//   }, {
+//     //prompt user for quantity of the item they want to purchase
+//     message: "What quantity would you like to purchase? ",
+//     type: "input",
+//     name: "quantity",
+//     validate: function (value) {
+//       if (isNaN(value) === false) {
+//         return true;
+//       }
+//       console.log(chalk.red('Please enter a number from 1-10')); //red text if NaN
+//       return false;
+//     }
+//   },
+//   {
+//     type: "confirm",
+//     message: "Are you sure?",
+//     name: "confirm",
+//     default: true
+//   },
+//   ]).then(function (answers) {
+
+//     var quantityNeeded = answers.quantity;
+//     var idNeeded = answers.productID;
+//     console.log(idNeeded);
+
+//     connection.query("SELECT * FROM products WHERE id = " + idNeeded, function (err, res) {
+//       if (err) throw err;
+//       console.log("Here is your order information and our current stock.");
+//       console.log(res);
+//     });
+//     })
+// }
 
 
 
