b38962f30a7e29b10dac633c8482f2f806f9b79d
Nenye <chinenyediei@gmail.com>
2019-07-13 05:28:12 -0500

    complete and working ready to post to portfolio and bootcampspot

--- javascript/bamazonCustomer.js
+++ javascript/bamazonCustomer.js
@@ -1,7 +1,5 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var express = require("express");
-var app = express();
 //Use table to display table
 var Table = require("cli-table");
 //chalk for error message
@@ -12,13 +10,10 @@ var PORT = process.env.PORT || 8080
 // create the connection information for the sql database
 var connection = mysql.createConnection({
   host: "localhost",
-
   // Your port; if not 3306
   PORT: 3306,
-
   // Your username
   user: "root",
-
   // Your password
   password: "Topper333",
   database: "bamazon_db"
@@ -31,8 +26,6 @@ connection.connect(function (err) {
   displayProducts();
   // afterConnection(); 
 });
-
-
 //display PRODUCTS AVAILABLE.
 function displayProducts() {
   connection.query("Select * FROM products", function (err, res) {
@@ -50,10 +43,10 @@ function displayProducts() {
     }
     console.log(displayTable.toString());
     // productPrompt(); 
-    userChoice();
+    
+userChoice(); 
   });
 }
-
 function userChoice(){
   // query the database for all items
   connection.query("SELECT * FROM products", function (err, results) {
@@ -70,7 +63,7 @@ function userChoice(){
             }
             return choiceArray;
           },
-          message: "What product you would like to purchase?"
+          message: "What product you would like to purchase (press CTRL + c to EXIT)?"
         },
         {
           type: "input",
@@ -88,7 +81,6 @@ function userChoice(){
       .then(function (answer) {
         var quantityNeeded = answer.quantity;
         var productName = answer.productID;
-        console.log("This is" + productName); 
         // get the information of the chosen item
         var chosenItem;
         for (var i = 0; i < results.length; i++) {
@@ -102,7 +94,7 @@ function userChoice(){
           //calculte total purchase price
           var productPrice = chosenItem.price;
           var cost = quantityNeeded * productPrice;
-          console.log("Your total is $" + cost);
+          console.log("Your total is $" + cost.toFixed(2));
           //update stock in db
           var updatedStock = chosenItem.stock_quantity - quantityNeeded;
           console.log(updatedStock);
@@ -117,66 +109,32 @@ function userChoice(){
             ],
             function (error) {
               if (error) throw err;
-              displayProducts();
+              //shows table again with updated quantity after purchase
+              // displayProducts();
+              reprompt();
             })
         }
         else {
-          // bid wasn't high enough, so apologize and start over
+          //quantity not available
           console.log("We don't have enough stock available please enter a smaller quantity...");
+          userChoice(); 
         }
       });
   });
 }
 
-
-
-// //prompt user for id# of item they want to purchase
-// var productPrompt = function (res) {
-//   inquirer.prompt([{
-//     message: "What? product you would like to purchase ",
-//     type: "input",
-//     name: "productID",
-//     validate: function (value) {
-//       if (isNaN(value) === false) {
-//         return true;
-//       }
-//       console.log(chalk.red('Please enter a number from 1-10'));
-//       return false;
-//     }
-//   }, {
-//     //prompt user for quantity of the item they want to purchase
-//     message: "What quantity would you like to purchase? ",
-//     type: "input",
-//     name: "quantity",
-//     validate: function (value) {
-//       if (isNaN(value) === false) {
-//         return true;
-//       }
-//       console.log(chalk.red('Please enter a number from 1-10')); //red text if NaN
-//       return false;
-//     }
-//   },
-//   {
-//     type: "confirm",
-//     message: "Are you sure?",
-//     name: "confirm",
-//     default: true
-//   },
-//   ]).then(function (answers) {
-
-//     var quantityNeeded = answers.quantity;
-//     var idNeeded = answers.productID;
-//     console.log(idNeeded);
-
-//     connection.query("SELECT * FROM products WHERE id = " + idNeeded, function (err, res) {
-//       if (err) throw err;
-//       console.log("Here is your order information and our current stock.");
-//       console.log(res);
-//     });
-//     })
-// }
-
-
-
-
+//asks if they would like to purchase another item
+function reprompt(){
+  inquirer.prompt([{
+    type: "confirm",
+    name: "reply",
+    message: "Would you like to purchase another item?"
+  }]).then(function(ans){
+    if(ans.reply){
+      userChoice();
+    } else{
+      console.log("See you soon!");
+    }
+  });
+}
 
